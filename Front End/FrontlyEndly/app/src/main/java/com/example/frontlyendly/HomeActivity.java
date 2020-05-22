116abe9e97841fe7cb0b76eecffe62f3365b02d9
ktrom <kylemtrom@gmail.com>
2020-05-21 23:38:56 -0500

    attempting communication between back end and front end

--- Front End/FrontlyEndly/app/src/main/java/com/example/frontlyendly/HomeActivity.java
+++ Front End/FrontlyEndly/app/src/main/java/com/example/frontlyendly/HomeActivity.java
@@ -3,13 +3,44 @@ package com.example.frontlyendly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class HomeActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
+        textView = (TextView)findViewById(R.id.test_text);
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String url ="192.168.1.6:3306/test";
+//
+//        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        textView.setText("Response is: "+ response.substring(0,500));
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                TextView textView =  (TextView)findViewById(R.id.test_text);
+//                textView.setText("That didn't work!");
+//            }
+//        });
+//
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest);
+//
     }
+
 }
